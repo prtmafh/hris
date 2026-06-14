885a26e12b2042122e1cc72832ebe58174e15a09
prtmafh <afilahprtm26@gmail.com>
2026-06-14 13:17:11 +0700

    edit filesistem

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,13 +38,21 @@ return [
             'report' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'root' => env('FILESYSTEM_PUBLIC_ROOT', public_path('storage')),
+            'url' => env('FILESYSTEM_PUBLIC_URL', env('APP_URL') . '/storage'),
             'visibility' => 'public',
-            'throw' => false,
             'report' => false,
+            'throw' => false,
         ],
 
         's3' => [
